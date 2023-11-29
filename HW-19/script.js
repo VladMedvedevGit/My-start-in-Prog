f4839af49409c7bbfcfee22e41e6b5bd73caea1e
VladMedvedevGit <shomer21@gmail.com>
2023-11-29 14:48:28 +0200

    HW-19_fixed

--- HW-19/script.js
+++ HW-19/script.js
@@ -1,40 +1,36 @@
 
 let userMinutes= document.querySelector('#minutes');
 userMinutes.placeholder= "00";
-
 let userSeconds= document.querySelector('#seconds');
 userSeconds.placeholder= "00";
-
 let startBtn= document.querySelector('.start'); 
 let resetBtn= document.querySelector('.reset'); 
-let counterInterval; 
-startBtn.addEventListener('click', function(){
-    
-        function delay(unit){ 
-            let counter= +unit.value; 
-            if(counter > 10){
-                counter -=1;          
-                unit.value=counter; 
-            console.log(counter);
-            } else if(counter < 11 && counter > 0){
-                counter -=1          
-                unit.value= `0${counter}`; 
-                console.log(counter);
-                } 
-        }   
-        if (+userMinutes.value == 0 && +userSeconds.value > 0 && +userSeconds.value <= 60){
-           setInterval(delay, 1000, userSeconds);     
-        };
-        
-        // if (+userMinutes.value > 0 || +userSeconds.value > 0){
-        //     setInterval(delay, 1000 * 60, userMinutes);
-        //    for (let i=60; i>0; i-=1) {
-        //     setInterval(delay, 1000, userSeconds);
-        //    } 
-        // } 
+let secInterval;
+startBtn.addEventListener('click', function(){    
+    function delay(unit,unit2){ 
+        let counter= +unit.value; 
+        if(counter > 10){
+            counter -=1;          
+            unit.value=counter; 
+        } else if(counter < 11 && counter > 0){
+            counter -=1;          
+            unit.value= `0${counter}`; 
+        } else if(+unit2.value >0 && counter == 0){
+            counter = 60
+            unit2.value --;
+            counter -=1;   
+            unit.value=counter;        
+            if(+unit2.value < 10 && +unit2.value >= 0){
+                unit2.value= `0${ unit2.value}`; 
+            }
+        }
+    }; 
+    if (+userSeconds.value > 0 && +userSeconds.value <= 60){
+        secInterval= setInterval(delay, 1000, userSeconds, userMinutes )
+    };       
 });
 resetBtn.addEventListener('click', function(){
-    clearInterval(counterInterval);
+    clearInterval(secInterval);
     userMinutes.value= "";
     userSeconds.value= ""; 
     userMinutes.placeholder= "00";  
